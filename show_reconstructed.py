47197feae843af38f82d88c8ffe11b65859558ce
Anis <73116724+anismaysami@users.noreply.github.com>
2023-02-24 16:44:07 +0330

    Update show_reconstructed.py

--- show_reconstructed.py
+++ show_reconstructed.py
@@ -9,14 +9,16 @@ def reconstruction(model, index, data, realmua, width, height):
 
     
     #display predicted by our model
-    fig,ax = plt.subplots(1,2)
-    ax[0,0].imshow(mua_hat[index].reshape(width,height))
+    plt.subplot(121)
+    plt.imshow(mua_hat[index].reshape(width,height))
     title_1=print(f"Predicted by {model}")
-    ax[0,0].set_title(title_1)
+    plt.title(title_1)
+    plt.colorbar
     
     #displays ground truth
     maximum_value_mua=max(realmua)
-    ax[0,1].imshow(realmua.reshape(width,height))
+    plt.subplot(122)
+    plt.imshow(realmua.reshape(width,height))
     title_2='Ground truth compared to {} - \u03BCa={} mm{}'.format(model, maximum_value_mua, get_super("-1"))
-    ax[0,1].title(title_2)
-    fig.colorbar()
+    plt.title(title_2)
+    plt.colorbar()

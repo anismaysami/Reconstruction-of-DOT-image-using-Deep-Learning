1127aa584a2b18e9b0cb14ca5d7b3f17751d94da
Anis <73116724+anismaysami@users.noreply.github.com>
2023-02-24 15:45:12 +0330

    chaning tiltle_2

--- show_reconstructed.py
+++ show_reconstructed.py
@@ -5,10 +5,10 @@ from superscript import get_super
 def reconstruction(model, index, data, realmua, width, height):
     #data is our measurement
     #realmua is real mua
-    mua_hat=model.predict(data) # mua_MLP_hat is predicted mua by MLP_model
+    mua_hat=model.predict(data) # mua_hat is predicted mua by MLP_model
 
     
-    #display predicted by MLP_model
+    #display predicted by our model
     plt.subplot(1,2)
     plt.imshow(mua_hat[index].reshape(width,height))
     title_1=print(f"Predicted by {model}")
@@ -18,6 +18,6 @@ def reconstruction(model, index, data, realmua, width, height):
     #displays ground truth
     maximum_value_mua=max(realmua)
     plt.imshow(realmua.reshape(width,height))
-    title_2='Ground truth compared to MLP model - \u03BCa={} mm{}'.format(maximum_value_mua, get_super("-1"))
+    title_2='Ground truth compared to {} - \u03BCa={} mm{}'.format(model, maximum_value_mua, get_super("-1"))
     plt.title(title_2)
     plt.colorbar()
